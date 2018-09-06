1c959aa200077373310224782e5af25e1dbe6364
saf0911 <saf0911@gmail.com>
2018-09-06 14:35:06 -0500

    setting up header and index page

--- pages/index.js
+++ pages/index.js
@@ -1,89 +1,94 @@
 import React from 'react'
 import Link from 'next/link'
 import Head from '../components/head'
-import Nav from '../components/nav'
+import Header from '../components/Header'
 
 const Home = () => (
-  <div>
-    <Head title="Home" />
-    <Nav />
+  <div className='page'>
+    <Head title="Paula" />
+    <Header />
 
     <div className="hero">
-      <h1 className="title">Welcome to Next!</h1>
-      <p className="description">
-        To get started, edit <code>pages/index.js</code> and save to reload.
-      </p>
-
-      <div className="row">
-        <Link href="https://github.com/zeit/next.js#getting-started">
-          <a className="card">
-            <h3>Getting Started &rarr;</h3>
-            <p>Learn more about Next on Github and in their examples</p>
-          </a>
-        </Link>
-        <Link href="https://open.segment.com/create-next-app">
-          <a className="card">
-            <h3>Examples &rarr;</h3>
-            <p>
-              Find other example boilerplates on the{' '}
-              <code>create-next-app</code> site
-            </p>
-          </a>
-        </Link>
-        <Link href="https://github.com/segmentio/create-next-app">
-          <a className="card">
-            <h3>Create Next App &rarr;</h3>
-            <p>Was this tool helpful? Let us know how we can improve it</p>
+      <div className='leftSide'>
+        <h4> Recent Books </h4>
+        <Link className='cookbooks'>
+          <a>
+            <img />
           </a>
         </Link>
       </div>
+      <div className='centerMain'>
+        <h2 className='bookTitle'>Thank You For Smoking </h2>
+        <img className='currentBook' src='/static/images/thankyouforsmoking_cover16.jpg' />
+      </div>
+      <div className='rightSide'>
+        <img />Bacon
+        <Link className='quotes'>
+          <a>
+            <p>
+              I WRITE ABOUT THE ENDLESS ADVENTURE OF FOOD AND TRAVEL.
+            </p>
+          </a>
+        </Link>
+
+      </div>
+
     </div>
 
-    <style jsx>{`
+    <style jsx global>{`
+      .page {
+
+      }
+
       .hero {
-        width: 100%;
-        color: #333;
+        list-style-type: none;
+        text-decoration: none;
+        background-color: white;
+        display: grid;
+        grid-template-columns: 1fr 2fr 1fr;
+        grid-template-rows: repeat(2, 1fr);
+        grid-template-areas:
+          'leftSide center rightSide'
+          'cookbooks center quotes'
+        grid-gap: 10px 10px;
+        margin-top: 5%;
       }
-      .title {
-        margin: 0;
+
+      .leftSide {
+        // grid-area: leftSide;
         width: 100%;
-        padding-top: 80px;
-        line-height: 1.15;
-        font-size: 48px;
+        margin: 3%;
       }
-      .title,
-      .description {
+
+      .cookbooks {
+        grid-area: cookbooks;
+      }
+
+      .centerMain {
+        // grid-area: center;
+        width: 100%;
+        display: inline;
+      }
+
+      .bookTitle {
         text-align: center;
       }
-      .row {
-        max-width: 880px;
-        margin: 80px auto 40px;
-        display: flex;
-        flex-direction: row;
-        justify-content: space-around;
+
+      .currentBook {
+        width: 100%;
+        align-self: center;
       }
-      .card {
-        padding: 18px 18px 24px;
-        width: 220px;
-        text-align: left;
-        text-decoration: none;
-        color: #434343;
-        border: 1px solid #9b9b9b;
+
+      .rightSide {
+        // grid-area: rightSide;
+        width: 100%;
+        display: inline;
       }
-      .card:hover {
-        border-color: #067df7;
+
+      .quotes {
+        grid-area: quotes;
       }
-      .card h3 {
-        margin: 0;
-        color: #067df7;
-        font-size: 18px;
-      }
-      .card p {
-        margin: 0;
-        padding: 12px 0 0;
-        font-size: 13px;
-        color: #333;
-      }
+
     `}</style>
   </div>
 )
